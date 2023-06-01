bafe27274c6df9b64423bf61783b18bceb8de211
Mathew Payne <geekmasher@gmail.com>
2023-06-01 12:16:58 +0100

    Add tests

---
+++ tests/test_cocoapods.py
@@ -0,0 +1,41 @@
+
+import unittest
+
+from ghastoolkit import Dependencies
+from cpdsa.cocoapods import createPod, parsePod
+
+class TestCocoaPods(unittest.TestCase):
+    def test_parse_pod(self):
+        data = 'YogaKit (1.0.0)'
+        deps = createPod(Dependencies(), data)
+
+        self.assertEqual(len(deps), 1)
+        dep = deps.pop(0)
+        self.assertEqual(dep.name, "YogaKit")
+        self.assertEqual(dep.version, "1.0.0")
+
+    def test_parse_pods(self):
+        data = {'YogaKit (1.18.1)': ["Yoga (~> 1.14)"]}
+        deps = createPod(Dependencies(), data)
+
+        self.assertEqual(len(deps), 2)
+
+        dep1 = deps.pop(0)
+        self.assertEqual(dep1.name, "YogaKit")
+        self.assertEqual(dep1.version, "1.18.1")
+
+        dep2 = deps.pop(0)
+        self.assertEqual(dep2.name, "Yoga")
+        self.assertEqual(dep2.version, "1.14")
+
+    def test_no_version(self):
+        data = "Flipper-Boost-iOSX"
+        dep = parsePod(data)
+        self.assertEqual(dep.name, "Flipper-Boost-iOSX")
+        self.assertEqual(dep.version, None)
+
+    def test_parse_equalversion(self):
+        data = "RCT-Folly (= 2021.07.22.00)"
+        dep = parsePod(data)
+        self.assertEqual(dep.name, "RCT-Folly")
+        self.assertEqual(dep.version, "2021.07.22.00")
