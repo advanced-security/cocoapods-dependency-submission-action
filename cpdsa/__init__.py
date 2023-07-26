79d0f01306eebed2dc6adba6e63f069c728b8043
Mathew Payne <2772944+GeekMasher@users.noreply.github.com>
2023-07-26 22:39:32 +0100

    Update __init__.py

--- cpdsa/__init__.py
+++ cpdsa/__init__.py
@@ -1,5 +1,5 @@
 __name__ = "cpdsa"
 __title__ = "cocoapods-dependency-submission-action"
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __url__ = "https://github.com/GeekMasher/cocoapods-dependency-submission-action"
